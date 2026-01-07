08af2903458f823b45057095bf7db27fa8e47847
Ajat Darojat <ajatdarojat45@gmail.com>
2026-01-07 19:43:52 +0700

    feat: enhance SurfaceMaterialList with create, update, and copy functionalities for materials

--- src/components/features/simulationSettings/SurfaceMaterialList.tsx
+++ src/components/features/simulationSettings/SurfaceMaterialList.tsx
@@ -7,11 +7,19 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import { Search } from "lucide-react";
+import { Search, SquarePen, Copy } from "lucide-react";
 import { useState } from "react";
-import { useGetMaterialsQuery } from "@/store/materialsApi";
+import {
+  useCreateMaterialMutation,
+  useGetMaterialsQuery,
+  useUpdateMaterialMutation,
+} from "@/store/materialsApi";
 import type { Material } from "@/types/material";
 import { MaterialFormDialog } from "./MaterialFormDialog";
+import { toast } from "sonner";
+import { useSelector } from "react-redux";
+import type { RootState } from "@/store";
+import { useUpdateSimulationMutation } from "@/store/simulationApi";
 
 type IProps = {
   openMaterialLibrary: boolean;
@@ -29,10 +37,72 @@ export function SurfaceMaterialList({
   const [selectedMaterial, setSelectedMaterial] = useState<Material | null>(null);
   const [searchQuery, setSearchQuery] = useState("");
   const { data: materials = [], isLoading, error } = useGetMaterialsQuery();
+  const [selectedMaterialForEdit, setSelectedMaterialForEdit] = useState<Material | null>(null);
+  const [createMaterial, { isLoading: isCreating }] = useCreateMaterialMutation();
+  const [isOpenEditForm, setIsOpenEditForm] = useState(false);
+  const [updateMaterial, { isLoading: isUpdating }] = useUpdateMaterialMutation();
+  const [isOpenCopyForm, setIsOpenCopyForm] = useState(false);
+  const activeSimulation = useSelector((state: RootState) => state.simulation.activeSimulation);
+  const [updateSimulation] = useUpdateSimulationMutation();
 
   const filteredMaterials = materials.filter((material) =>
     material.name.toLowerCase().includes(searchQuery.toLowerCase()),
   );
+
+  const handleCreate = async (material: Omit<Material, "id" | "createdAt" | "updatedAt">) => {
+    try {
+      await createMaterial(material).unwrap();
+      toast.success("Material created successfully!");
+      setOpenCreateMaterialDialog(false);
+    } catch (error) {
+      toast.error("Failed to create material");
+      console.error("Error creating material:", error);
+    }
+  };
+
+  const handleUpdate = async (material: Omit<Material, "createdAt" | "updatedAt" | "id">) => {
+    try {
+      await updateMaterial({ id: selectedMaterialForEdit?.id as number, ...material }).unwrap();
+      toast.success("Material updated successfully!");
+      setIsOpenEditForm(false);
+    } catch (error) {
+      toast.error("Failed to update material");
+      console.error("Error updating material:", error);
+    }
+  };
+
+  const handleCopy = async (material: Omit<Material, "id" | "createdAt" | "updatedAt">) => {
+    try {
+      const newMaterial = await createMaterial(material).unwrap();
+
+      const layerIdByMaterialId: Record<string, number> = {};
+      for (const key in activeSimulation?.layerIdByMaterialId) {
+        if (activeSimulation?.layerIdByMaterialId[key] === selectedMaterialForEdit?.id) {
+          layerIdByMaterialId[key] = newMaterial.id;
+        } else {
+          layerIdByMaterialId[key] = activeSimulation?.layerIdByMaterialId[key];
+        }
+      }
+
+      const payload = {
+        id: activeSimulation?.id as number,
+        body: {
+          modelId: activeSimulation?.modelId as number,
+          name: activeSimulation?.name,
+          status: activeSimulation?.status,
+          hasBeenEdited: true,
+          layerIdByMaterialId: layerIdByMaterialId,
+        },
+      };
+      await updateSimulation(payload).unwrap();
+
+      toast.success("Material copied successfully!");
+      setIsOpenCopyForm(false);
+    } catch (error) {
+      toast.error("Failed to copy material");
+      console.error("Error copying material:", error);
+    }
+  };
 
   return (
     <Dialog open={openMaterialLibrary} onOpenChange={setOpenMaterialLibrary}>
@@ -45,8 +115,34 @@ export function SurfaceMaterialList({
             <div className="flex justify-between items-center mt-4">
               <DialogTitle className="text-xl text-choras-primary">Material library</DialogTitle>
               <MaterialFormDialog
-                openCreateMaterialDialog={openCreateMaterialDialog}
-                setOpenCreateMaterialDialog={setOpenCreateMaterialDialog}
+                isOpen={openCreateMaterialDialog}
+                onOpen={setOpenCreateMaterialDialog}
+                label={"Create"}
+                description={"Fill in the details to create a new material."}
+                onSubmit={handleCreate}
+                isLoading={isCreating}
+              />
+
+              <MaterialFormDialog
+                isOpen={isOpenEditForm}
+                onOpen={() => setIsOpenEditForm(!isOpenEditForm)}
+                material={selectedMaterialForEdit}
+                label={"Update"}
+                description={"Update the details of the material."}
+                onSubmit={handleUpdate}
+                isLoading={isUpdating}
+                isShownTrigger={false}
+              />
+
+              <MaterialFormDialog
+                isOpen={isOpenCopyForm}
+                onOpen={() => setIsOpenCopyForm(!isOpenCopyForm)}
+                material={selectedMaterialForEdit}
+                label={"Copy"}
+                description={"Copy the details of the material to create a new one."}
+                onSubmit={handleCopy}
+                isLoading={isUpdating}
+                isShownTrigger={false}
               />
             </div>
             <DialogDescription>Select a material to view its details</DialogDescription>
@@ -110,6 +206,29 @@ export function SurfaceMaterialList({
                               </p>
                             )}
                           </div>
+                          {material.origin === "user" ? (
+                            <SquarePen
+                              size={16}
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                setSelectedMaterialForEdit(material);
+                                setIsOpenEditForm(true);
+                              }}
+                            />
+                          ) : (
+                            <Copy
+                              size={16}
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                const payload = {
+                                  ...material,
+                                  name: "Copy of " + material.name,
+                                };
+                                setSelectedMaterialForEdit(payload);
+                                setIsOpenCopyForm(true);
+                              }}
+                            />
+                          )}
                         </div>
                       ))}
                     </div>
